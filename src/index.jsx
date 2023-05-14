b91e7ea15da0da14812892634675902e6c1b0e0a
jacksonpickard <jackpickard2@gmail.com>
2023-05-14 15:36:47 -0600

    Getting error "react is not defined"

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 
-import React from 'react';
+// import React from 'react';
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-const MyFlixApplication = () => {
+const App = () => {
   return <MainView />;
 };
 
@@ -16,4 +17,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
