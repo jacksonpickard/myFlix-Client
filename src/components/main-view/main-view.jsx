4a195888627cc355db12bb3d18d228f9b6b28405
jacksonpickard <jackpickard2@gmail.com>
2023-08-07 16:05:35 -0600

    3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,50 +1,89 @@
-import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import MovieView from './MovieView';
 
-export const MainView = () => {
-    const [movies, setMovies] = useState ([]);
+function MainView() {
+	const [movies, setMovies] = useState([]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+	const [selectedMovie, setSelectedMovie] = useState(null);
 
-    useEffect(() => {
-        fetch("https://jacksons-movie-api.herokuapp.com/")
-        .then(( response) => response.json())
-        .then((data) => {
-            const moviesFromApi = data.map((movie) => {
-                return {
-                    id: movie.key,
-                    Title: movie.Title,
-                    ImageURL: movie.ImageURL,
-                    Director: movie.Director
-                };
-            });
+	useEffect(() => {
+		const fetchMovieData = async () => {
+			const fetchedData = await fetch("https://jacksons-movie-api.herokuapp.com/");
+			const data = await fetchedData.json();
+			const moviesFromAPI = data.map((movie) => {
+				return {
+					id: movie._id,
+					title: movie.Title,
+					image: movie.ImagePath,
+					description: movie.Description,
+					actors: movie.Actors,
+					genre: {
+						name: movie.Genre.Name,
+						description: movie.Genre.Description,
+					},
+					director: {
+						name: movie.Director.Name,
+						bio: movie.Director.Bio,
+					},
+				};
+			});
 
-            setMovies(moviesFromApi);
-        });
-    }, []);
+			setMovies(moviesFromAPI);
+		};
 
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-    }
+		fetchMovieData();
+	}, []);
 
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
+	// Display selected movie details and similar movie cards
+	function displayMovieView() {
+		let similarMovies = movies.filter((movie) => {
+			return movie.id !== selectedMovie.id && movie.genre.name == selectedMovie.genre.name;
+		});
 
-    return (
-        <div>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={movie.id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))}
-        </div>
-    );
-};
+		return (
+			<>
+				<MovieView
+					movie={selectedMovie}
+					onBackClick={() => {
+						setSelectedMovie(null);
+					}}
+				/>
+				<br />
+				<h2>Similar Movies</h2>
+				{similarMovies.map((movie) => (
+					<MovieCard
+						key={movie.id}
+						movie={movie}
+						onClick={(newSelectedMovie) => {
+							setSelectedMovie(newSelectedMovie);
+						}}
+					/>
+				))}
+			</>
+		);
+	}
+
+	// Display MovieView if there is a selected movie, and display MovieCard list if there is none selected.
+	return (
+		<div>
+			{selectedMovie ? (
+				displayMovieView()
+			) : movies.length ? (
+				movies.map((movie) => (
+					<MovieCard
+						key={movie.id}
+						movie={movie}
+						onClick={(newSelectedMovie) => {
+							setSelectedMovie(newSelectedMovie);
+						}}
+					/>
+				))
+			) : (
+				<div>The Movie list is empty!</div>
+			)}
+		</div>
+	);
+}
+
+export default MainView;
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,30 +1,46 @@
-export const MovieView = ({ movie, onBackClick }) => {
-    return (
-        <div>
-            <div>
-                <img src={movie.ImageURL} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director}</span>
-            </div>
-            <div>
-                <span>Year: </span>
-                <span>{movie.Year}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
+import PropTypes from 'prop-types';
+
+function MovieView({ movie, onBackClick }) {
+	return (
+		<div>
+			<div>
+				<img src={movie.image} className='movie-img' />
+			</div>
+			<div>
+				<span>Title: </span>
+				<span>{movie.title}</span>
+			</div>
+			<div>
+				<span>Description: </span>
+				<span>{movie.description}</span>
+			</div>
+			<div>
+				<span>Genre: </span>
+				<span>{movie.genre.name}</span>
+			</div>
+			<div>
+				<span>Director: </span>
+				<span>{movie.director.name}</span>
+			</div>
+			<button onClick={onBackClick}>Back</button>
+		</div>
+	);
+}
+
+// Define the prop types this component should have
+MovieView.propTypes = {
+	movie: PropTypes.shape({
+		image: PropTypes.string,
+		title: PropTypes.string,
+		description: PropTypes.string.isRequired,
+		genre: PropTypes.shape({
+			name: PropTypes.string,
+		}),
+		director: PropTypes.shape({
+			name: PropTypes.string,
+		}),
+	}).isRequired,
+	onBackClick: PropTypes.func.isRequired,
 };
+
+export default MovieView;
