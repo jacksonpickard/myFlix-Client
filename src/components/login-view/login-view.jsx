abb4d1db800a18c1147e61f75e0b5ab6836c1022
jacksonpickard <jackpickard2@gmail.com>
2023-08-16 14:02:30 -0700

    3.6

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,61 +1,75 @@
-import { useState} from "react";
+import React from "react"
+import { useState } from "react"
+import { Form, Button } from "react-bootstrap"
 
-export const LoginView =    ({onLoggedIn}) => {
-
-    const [username, setUsername] = useState("");
+export const LoginView = ({ onLoggedIn }) => {
+    const [userName, setuserName] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
+    const loginSubmitHandler = (event) => {
         event.preventDefault();
-
-        const data = {
-            Username: username,
-            Password: password
+        const reqBody = {
+            userName: userName,
+            password: password
         };
 
         fetch("https://jacksons-movie-api.herokuapp.com/login", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("Login response: ", data);
-        if (data.user) {
-            onLoggedIn (data.user, data.token);
-        } else {
-           alert("no such user");  
-        }
-    })
-    .catch((e) => {
-        alert("something went wrong");
-    });
-};
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(reqBody)
+        }).then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
 
-return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token)
+                    onLoggedIn(data.user, data.token);
+                    window.location.reload();
+                } else {
+                    alert("User not found");
+                }
+
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            })
+    }
+
+    return (
+        <>
+            <div className="logIn">
+                <Form className="m-5" onSubmit={loginSubmitHandler}>
+                    <h3>Login</h3>
+                    <Form.Group controlId="loginUsername">
+                        <Form.Label className="text-lg mt-3">Username:</Form.Label>
+                        <Form.Control className={"bg-light"}
+                            type="text"
+                            size="lg"
+                            value={userName}
+                            onChange={(e) => setuserName(e.target.value)}
+                            required
+                            minLength="5" />
+                    </Form.Group>
+
+                    <Form.Group controlId="loginPassword">
+                        <Form.Label className="text-lg mt-3">Password:</Form.Label>
+                        <Form.Control className={"bg-light"}
+                            type="password"
+                            size="lg"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            minLength="6" />
+                    </Form.Group>
+                    <Button className="mt-2" type="submit">Submit</Button>
+                </Form>
+
+            </div >
+        </>)
+
 }
 
 
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,46 +1,26 @@
 import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
 
-function MovieView({ movie, onBackClick }) {
-	return (
-		<div>
-			<div>
-				<img src={movie.image} className='movie-img' />
-			</div>
-			<div>
-				<span>Title: </span>
-				<span>{movie.title}</span>
-			</div>
-			<div>
-				<span>Description: </span>
-				<span>{movie.description}</span>
-			</div>
-			<div>
-				<span>Genre: </span>
-				<span>{movie.genre.name}</span>
-			</div>
-			<div>
-				<span>Director: </span>
-				<span>{movie.director.name}</span>
-			</div>
-			<button onClick={onBackClick}>Back</button>
-		</div>
-	);
+export const MovieView = ({ selectedMovie }) => {
+    return (
+        <>
+            <Card style={{ maxWidth: "50rem" }}>
+                <Card.Img className="img-fluid" src={selectedMovie.image} alt="movieImg" />
+            </Card>
+            <div className='movieDescriptionHeader'><span>Title: </span>{selectedMovie.title}</div>
+            <div className='movieDescriptionHeader'><span>Director: </span > {selectedMovie.director}</div >
+            <div className='movieDescriptionHeader'>{selectedMovie.directorBio}</div>
+            <div className='movieDescriptionHeader'><span>Description: </span > {selectedMovie.genreDescription}</div >
+            <div className='movieDescriptionHeader'>{selectedMovie.description}</div>
+        </>
+    )
 }
 
-// Define the prop types this component should have
 MovieView.propTypes = {
-	movie: PropTypes.shape({
-		image: PropTypes.string,
-		title: PropTypes.string,
-		description: PropTypes.string.isRequired,
-		genre: PropTypes.shape({
-			name: PropTypes.string,
-		}),
-		director: PropTypes.shape({
-			name: PropTypes.string,
-		}),
-	}).isRequired,
-	onBackClick: PropTypes.func.isRequired,
-};
-
-export default MovieView;
+    selectedMovie: PropTypes.shape({
+        title: PropTypes.string,
+        description: PropTypes.string,
+        image: PropTypes.string
+    }).isRequired,
+    onBackButtonClick: PropTypes.func.isRequired
+}
